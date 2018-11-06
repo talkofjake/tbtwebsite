6a464d2a864020368f6d1463211f243da72573b7
Jake <talkofjake@gmail.com>
2018-11-05 19:45:22 -0500

    intial commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function greeting(){
+    alert('Hello! We are Throwback Thursday. Louisville pop punk party band!')
+    alert('For booking information, contact Rob Saag at tbtlouisville@gmail.com');
+}
+greeting();
+
+function forBooking(name) {
+    alert("Thank you for visitng " + name + ". Contact Rob Saag at tbtlouisville@gmail.com for all of your party needs!");
+}
